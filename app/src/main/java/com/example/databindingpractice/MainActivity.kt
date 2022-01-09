ea535d42ba78b36307c6eadabdaf257695cab9f6
YutaroM <yutaroapp@gmail.com>
2022-01-09 16:27:15 +0900

    レイアウトの修正（下準備）

--- app/src/main/java/com/example/databindingpractice/MainActivity.kt
+++ app/src/main/java/com/example/databindingpractice/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.databindingpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.databindingpractice.Fragment.TextInputFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, TextInputFragment())
+            .commit()
     }
 }
